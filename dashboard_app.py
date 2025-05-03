a1ddf2423504de0706d636f334de715a5db1f1e0
Valentine Ghanem <154077429+vghanem@users.noreply.github.com>
2025-05-03 21:49:27 +0000

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 # Load and display the logo in the sidebar
 logo = Image.open("image_1.png")
-st.sidebar.image(logo, use_column_width=True)
+st.sidebar.image(logo, use_container_width=True)
 import pandas as pd
 import geopandas as gpd
 import plotly.express as px
